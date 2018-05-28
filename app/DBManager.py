4f27564b0a6961b4fb35d116667aede4276652d6
201411317 <whalsrb1226@naver.com>
2018-05-28 16:00:15 +0900

    Database Create Table Complete

--- app/DBManager.py
+++ app/DBManager.py
@@ -4,7 +4,24 @@ class DBManager:
 
     def __init__(self):
         self.conn = sqlite3.connect("Kurrent.db")
-        self.cur = conn.cursor()
+        self.cur = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        try:
+            sql = 'CREATE TABLE User(UUID VARCHAR(36) PRIMARY KEY,IP VARCHAR(15),PORT INTEGER)'
+            self.cur.execute(sql)
+            sql = 'CREATE TABLE Seeder(UserIdx VARCHAR(36), FileIdx VARCHAR(36), FOREIGN KEY (UserIdx) REFERENCES User (UUID))'
+            self.cur.execute(sql)
+            sql = 'CREATE TABLE Leecher(UserIdx VARCHAR(36), FileIdx VARCHAR(36), FOREIGN KEY (UserIdx) REFERENCES User (UUID))'
+            self.cur.execute(sql)
+            sql = 'CREATE TABLE HashTable(FileHash VARCHAR(36), UserIdx VARCHAR(36), BlockNum INTEGER, FOREIGN KEY (UserIdx) REFERENCES User (UUID))'
+            self.cur.execute(sql)
+
+            self.conn.commit()
+        except :
+            print('Table Already Created')
+
 
     def get_seeder_list(self, file_hash):
         sql = "select * from Seeder where FileHash=?"
